174f07faf588e84dfdf9af466f4c1db1d521d770
Vex Darkon <edanvirgo@gmail.com>
2019-03-08 14:07:08 -0700

    MORE FIXES

--- src/main/java/vexMod/relics/ShiftingSkin.java
+++ src/main/java/vexMod/relics/ShiftingSkin.java
@@ -2,13 +2,11 @@ package vexMod.relics;
 
 import basemod.abstracts.CustomRelic;
 import com.badlogic.gdx.graphics.Texture;
-import com.evacipated.cardcrawl.mod.stslib.relics.BetterOnLoseHpRelic;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.powers.ThornsPower;
 import vexMod.VexMod;
+import vexMod.actions.BlazeAction;
 import vexMod.util.TextureLoader;
 
 import static vexMod.VexMod.makeRelicOutlinePath;
@@ -29,28 +27,9 @@ public class ShiftingSkin extends CustomRelic {
     }
 
     public void atBattleStart() {
-        this.GAINED = 0;
-    }
-
-    // Gain 1 thorn for each HP lost.
-    @Override
-    public int onAttacked(DamageInfo info, int damageAmount) {
-        if (info.owner != null && info.type == DamageInfo.DamageType.NORMAL && damageAmount > 0)
-        {
-            this.flash();
-            if (GAINED > 0)
-            {
-                AbstractDungeon.player.getPower(ThornsPower.POWER_ID).stackPower(this.GAINED * -1);
-            }
-            AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new ThornsPower(AbstractDungeon.player, damageAmount), damageAmount));
-            GAINED = damageAmount;
-        }
-        return damageAmount;
-    }
-
-    @Override
-    public void onVictory() {
-        this.GAINED = 0;
+        this.flash();
+        AbstractDungeon.actionManager.addToBottom(new BlazeAction(AbstractDungeon.player, 1));
+        AbstractDungeon.actionManager.addToBottom(new RelicAboveCreatureAction(AbstractDungeon.player, this));
     }
 
     // Description
